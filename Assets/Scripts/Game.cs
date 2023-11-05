f592b67502636427d5b5abda2c84420018bd5d55
Nettakrim <106409257+Nettakrim@users.noreply.github.com>
2023-11-05 18:41:53 +0000

    shipping points now work

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -24,6 +24,9 @@ public class Game : MonoBehaviour
 
     private ComputeBuffer gateBuffer;
 
+    private int[] shipping = new int[24];
+    private ComputeBuffer shippingBuffer;
+
     void Start() {
         CreateRenderTexture(ref texA, 256, 256);
         CreateRenderTexture(ref texB, 256, 256);
@@ -43,14 +46,24 @@ public class Game : MonoBehaviour
         Reset();
     }
 
+    private void OnDestroy() {
+        //try to prevent memory leaks
+        Debug.Log("quitting");
+        if (gateBuffer != null) gateBuffer.Release();
+        if (shippingBuffer != null) shippingBuffer.Release();
+        texA.Release();
+        texB.Release();
+    }
+
     void Reset() {
         step = false;
 
         if (gateBuffer != null) gateBuffer.Release();
         gateBuffer = new ComputeBuffer(4, sizeof(int));
+        if (shippingBuffer != null) shippingBuffer.Release();
+        shippingBuffer = new ComputeBuffer(24, sizeof(int));
 
-        computeShader.SetBuffer(kernal3x3Start, "GateBuffer", gateBuffer);
-        computeShader.Dispatch(kernal3x3Start, 1, 1, 1);
+        Start3x3();
 
         computeShader.SetTexture(kernalInit, "Input", texA);
         computeShader.SetTexture(kernalInit, "Result", texB);
@@ -74,13 +87,31 @@ public class Game : MonoBehaviour
 
     void Simulate(RenderTexture i, RenderTexture o) {
         if (offsets.remaining == 0) {
-            computeShader.SetBuffer(kernal3x3Start, "GateBuffer", gateBuffer);
-            computeShader.Dispatch(kernal3x3Start, 1, 1, 1);
+            Start3x3();
         }
 
         SimulationStep(i, o, offsets.Get());
 
         material.SetFloat("_Step", step ? 1 : 0);
+    }
+
+    void Start3x3() {
+        AsyncGPUReadback.Request(shippingBuffer, OnCompleteShippingReadBack);
+
+        computeShader.SetBuffer(kernal3x3Start, "GateBuffer", gateBuffer);
+        computeShader.SetBuffer(kernal3x3Start, "ShippingBuffer", shippingBuffer);
+        computeShader.Dispatch(kernal3x3Start, 1, 1, 1);
+    }
+
+    void OnCompleteShippingReadBack(AsyncGPUReadbackRequest request) {
+        if (shippingBuffer != null) {
+            request.GetData<int>().CopyTo(shipping);
+            string s = "";
+            for (int i = 0; i < 24; i++) {
+                s += shipping[i]+(i < 23 ? " " : "");
+            }
+            Debug.Log(s);
+        }
     }
 
     void SimulationStep(RenderTexture i, RenderTexture o, int[] offset) {
@@ -89,6 +120,7 @@ public class Game : MonoBehaviour
         computeShader.SetInts("PosOffset", offset);
         computeShader.SetInts("Size", size);
         computeShader.SetBuffer(kernalStep, "GateBuffer", gateBuffer);
+        computeShader.SetBuffer(kernalStep, "ShippingBuffer", shippingBuffer);
 
         computeShader.Dispatch(kernalStep, Mathf.CeilToInt(i.width / 3f)+1, Mathf.CeilToInt(i.height / 3f)+1, 1);
     }
